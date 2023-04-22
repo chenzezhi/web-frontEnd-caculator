7d7b67e2cadc3e289b07adff56b9d07813afc11d
admin <2841989426@qq.com>
2023-04-22 08:18:53 +0800

    first commit

--- 新建文本文档.bat
+++ 新建文本文档.bat
@@ -1,6 +1,7 @@
 git init
 git add .
 git commit -m "first commit"
+git remote remove origin
 git remote add origin https://github.com/chenzezhi/web-frontEnd-caculator.git
 git branch -M main
 git push -f origin main
